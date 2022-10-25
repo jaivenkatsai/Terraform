d2af30a0d38a32d7a99cd25edecec6312111a517
jaivenkatsai <venkatpulivarthi88@gmail.com>
2022-10-25 14:37:39 +0530

    cluster creation

--- 0-providers.tf
+++ 0-providers.tf
@@ -1,12 +1,13 @@
 provider "aws" {
     region = "us-east-1"
+    profile = "terraform"
 }
 
-terraform {
-    required_providers {
-        aws = {
-            source = "hashicrop/aws"
-            version ="~> 3.0"
-        }
-    }
-}
+# terraform {
+#     required_providers {
+#         aws = {
+#             source = "hashicrop/aws"
+#             version ="~> 3.0"
+#         }
+#     }
+# }
